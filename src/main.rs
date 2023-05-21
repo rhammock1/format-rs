53e6d005f79aebaf145b2431a7d6b331d26f81d1
rhammock1 <robert.hammock@gotab.io>
2023-05-21 12:48:47 -0400

    Check for same closing tag as opening

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     io::{self, BufRead},
+    collections::HashMap,
     env,
     fs,
     path,
@@ -7,7 +8,7 @@ use std::{
 };
 
 use colored::Colorize;
-use regex::Regex;
+use fancy_regex::Regex;
 
 #[derive(Clone, Copy, Debug)]
 enum FormatType {
@@ -41,119 +42,144 @@ fn parse_args(arguments: &Vec<String>) -> Result<String, &'static str> {
 
 fn apply_styles(line: String) -> String {
     let mut styled_content = String::new();
-
     let mut previous_format_type = FormatType::Normal;
 
-    for character in line.chars() {
-        match character {
-            c if c.is_digit(10) => {
-                styled_content.push_str(&character.to_string().cyan().to_string());
-                previous_format_type = FormatType::Number;
-            },
-            '.' => {
-                match previous_format_type {
-                    FormatType::Number => {
-                        styled_content.push_str(&character.to_string().cyan().to_string());
-                    },
-                    _ => {
-                        styled_content.push_str(&character.to_string());
-                    }
-                }
-            },
-            '~' => {
-                match previous_format_type {
-                    FormatType::Strikethrough => {
-                        previous_format_type = FormatType::Normal;
-                    },
-                    _ => {
-                        previous_format_type = FormatType::Strikethrough;
-                    },
-                }
-            },
-            '`' => {
-                match previous_format_type {
-                    FormatType::Highlight => {
-                        previous_format_type = FormatType::Normal;
-                    },
-                    _ => {
-                        previous_format_type = FormatType::Highlight;
-                    },
-                }
-            },
-            '\'' => {
-                match previous_format_type {
-                    FormatType::SingleQuoteString => {
-                        previous_format_type = FormatType::Normal;
-                        styled_content.push_str(&character.to_string().yellow().to_string());
-                    },
-                    _ => {
-                        previous_format_type = FormatType::SingleQuoteString;
-                        styled_content.push_str(&character.to_string().yellow().to_string());
-                    },
-                }
-            },
-            '"' => {
-                match previous_format_type {
-                    FormatType::DoubleQuoteString => {
-                        previous_format_type = FormatType::Normal;
-                        styled_content.push_str(&character.to_string().bright_red().to_string());
+    // Char to FormatType map
+    let mut format_map: HashMap<char, FormatType> = HashMap::new();
 
-                    },
-                    _ => {
-                        previous_format_type = FormatType::DoubleQuoteString;
-                        styled_content.push_str(&character.to_string().bright_red().to_string());
-                    },
-                }
-            },
-            '*' => {
-                match previous_format_type {
-                    FormatType::Italicize => {
-                        previous_format_type = FormatType::Normal;
-                    },
-                    _ => {
-                        previous_format_type = FormatType::Italicize;
-                    },
-                }
-            },
-            '_' => {
-                match previous_format_type {
-                    FormatType::Bold => {
-                        previous_format_type = FormatType::Normal;
-                    },
-                    _ => {
-                        previous_format_type = FormatType::Bold;
-                    },
-                }
-            },
-            _ => {
-                match previous_format_type {
-                    FormatType::Highlight => {
-                        // TODO add a check if terminal supports truecolor
-                        styled_content.push_str(&character.to_string().on_truecolor(135, 28, 167).to_string());
-                    },
-                    FormatType::SingleQuoteString => {
-                        styled_content.push_str(&character.to_string().yellow().to_string());
-                    },
-                    FormatType::DoubleQuoteString => {
-                        styled_content.push_str(&character.to_string().bright_red().to_string());
-                    },
-                    FormatType::Italicize => {
-                        styled_content.push_str(&character.to_string().italic().to_string());
-                    },
-                    FormatType::Bold => {
-                        styled_content.push_str(&character.to_string().bold().to_string());
-                    },
-                    FormatType::Strikethrough => {
-                        styled_content.push_str(&character.to_string().strikethrough().to_string());
-                    },
-                    _ => {
-                        // No need for match arm to check FormatType::Number since the
-                        // match arm above handles checking if the char is a digit
-                        styled_content.push_str(&character.to_string());
-                    },
-                }
-            },
+    format_map.insert('\'', FormatType::SingleQuoteString);
+    format_map.insert('"', FormatType::DoubleQuoteString);
+    format_map.insert('~', FormatType::Strikethrough);
+    format_map.insert('`', FormatType::Highlight);
+    format_map.insert('*', FormatType::Italicize);
+    format_map.insert('_', FormatType::Bold);
+
+    // Matches the opening character and the same closing character
+    let format_pattern = format!(
+        r"([{0}{1}])[^{0}{1}]+(\1)",
+        fancy_regex::escape(&format_map.keys().collect::<String>()),
+        fancy_regex::escape(&format_map.keys().collect::<String>())
+    );
+
+    let pattern = Regex::new(&format_pattern).unwrap_or_else(|err| {
+        eprintln!("ERROR: Could not create regex pattern: {}", err);
+        process::exit(1);
+    });
+
+    if pattern.is_match(&line).unwrap() {
+        for character in line.chars() {
+            match character {
+                c if c.is_digit(10) => {
+                    styled_content.push_str(&character.to_string().cyan().to_string());
+                    previous_format_type = FormatType::Number;
+                },
+                '.' => {
+                    match previous_format_type {
+                        FormatType::Number => {
+                            styled_content.push_str(&character.to_string().cyan().to_string());
+                        },
+                        _ => {
+                            styled_content.push_str(&character.to_string());
+                        }
+                    }
+                },
+                '~' => {
+                    match previous_format_type {
+                        FormatType::Strikethrough => {
+                            previous_format_type = FormatType::Normal;
+                        },
+                        _ => {
+                            previous_format_type = FormatType::Strikethrough;
+                        },
+                    }
+                },
+                '`' => {
+                    match previous_format_type {
+                        FormatType::Highlight => {
+                            previous_format_type = FormatType::Normal;
+                        },
+                        _ => {
+                            previous_format_type = FormatType::Highlight;
+                        },
+                    }
+                },
+                '\'' => {
+                    match previous_format_type {
+                        FormatType::SingleQuoteString => {
+                            previous_format_type = FormatType::Normal;
+                            styled_content.push_str(&character.to_string().yellow().to_string());
+                        },
+                        _ => {
+                            previous_format_type = FormatType::SingleQuoteString;
+                            styled_content.push_str(&character.to_string().yellow().to_string());
+                        },
+                    }
+                },
+                '"' => {
+                    match previous_format_type {
+                        FormatType::DoubleQuoteString => {
+                            previous_format_type = FormatType::Normal;
+                            styled_content.push_str(&character.to_string().bright_red().to_string());
+
+                        },
+                        _ => {
+                            previous_format_type = FormatType::DoubleQuoteString;
+                            styled_content.push_str(&character.to_string().bright_red().to_string());
+                        },
+                    }
+                },
+                '*' => {
+                    match previous_format_type {
+                        FormatType::Italicize => {
+                            previous_format_type = FormatType::Normal;
+                        },
+                        _ => {
+                            previous_format_type = FormatType::Italicize;
+                        },
+                    }
+                },
+                '_' => {
+                    match previous_format_type {
+                        FormatType::Bold => {
+                            previous_format_type = FormatType::Normal;
+                        },
+                        _ => {
+                            previous_format_type = FormatType::Bold;
+                        },
+                    }
+                },
+                _ => {
+                    match previous_format_type {
+                        FormatType::Highlight => {
+                            // TODO add a check if terminal supports truecolor
+                            styled_content.push_str(&character.to_string().on_truecolor(135, 28, 167).to_string());
+                        },
+                        FormatType::SingleQuoteString => {
+                            styled_content.push_str(&character.to_string().yellow().to_string());
+                        },
+                        FormatType::DoubleQuoteString => {
+                            styled_content.push_str(&character.to_string().bright_red().to_string());
+                        },
+                        FormatType::Italicize => {
+                            styled_content.push_str(&character.to_string().italic().to_string());
+                        },
+                        FormatType::Bold => {
+                            styled_content.push_str(&character.to_string().bold().to_string());
+                        },
+                        FormatType::Strikethrough => {
+                            styled_content.push_str(&character.to_string().strikethrough().to_string());
+                        },
+                        _ => {
+                            // No need for match arm to check FormatType::Number since the
+                            // match arm above handles checking if the char is a digit
+                            styled_content.push_str(&character.to_string());
+                        },
+                    }
+                },
+            }
         }
+    } else {
+        styled_content.push_str(&line);
     }
 
     styled_content.push_str("\n");
